c2292e458639b3bc833c7769e1cdfece8db27542
Dyako Baram <55919885+dyako-baram@users.noreply.github.com>
2021-03-09 10:18:21 +0300

    basic setup with get request

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Bemol;
 
 namespace BemolSample
 {
@@ -6,7 +9,44 @@ namespace BemolSample
     {
         static void Main(string[] args)
         {
-            
+            App app=new App().Start(5000);
+            var personList=PopulatePersonList();
+
+            app.Get("/",(x)=>{
+                x.Json(personList);
+            });
+
+            app.Get("/:id",(x)=>{
+                int id=x.PathParam<int>("id");
+                x.Json(personList.FirstOrDefault(x=>x.Id==id));
+            });
+
         }
+        public static List<Person> PopulatePersonList()
+        {
+            return new List<Person>{
+                new Person(1,"john",25),
+                new Person(2,"jane",34),
+                new Person(3,"bob",12),
+                new Person(4,"jake",45),
+                new Person(5,"doe",22),
+                new Person(6,"sam",11),
+                new Person(7,"drake",26),
+                new Person(8,"naithen",66)
+            };
+        }
+        
     }
+    public class Person
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public Person(int id,string name,int age)
+            {
+                Id=id;
+                Name=name;
+                Age=age;
+            }
+        }
 }
