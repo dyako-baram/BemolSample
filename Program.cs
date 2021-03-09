2d1027f687e03de564e1c3a523a4a0f3211d3756
Dyako Baram <55919885+dyako-baram@users.noreply.github.com>
2021-03-09 10:16:09 +0300

    initialize console project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BemolSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# simple web api with bemol
+
